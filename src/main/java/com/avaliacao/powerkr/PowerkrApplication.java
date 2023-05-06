c929d5166e234aba38fd2ad960f15d7868415ec7
Allas Assis <allasassis10@gmail.com>
2023-05-05 23:25:43 -0300

    Projeto criado

---
+++ src/main/java/com/avaliacao/powerkr/PowerkrApplication.java
@@ -0,0 +1,13 @@
+package com.avaliacao.powerkr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerkrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowerkrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/avaliacao/powerkr/PowerkrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avaliacao.powerkr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerkrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
